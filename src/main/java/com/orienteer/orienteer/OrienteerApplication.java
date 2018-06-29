e367a8140f35f16f789182abeac13a57419da0b5
Kelly Villalobos <kvdesign1978@gmail.com>
2018-06-29 17:05:43 -0500

    finished geocache model and relationships, added a home.html and imported dependecies for OrienteerApplication

--- src/main/java/com/orienteer/orienteer/OrienteerApplication.java
+++ src/main/java/com/orienteer/orienteer/OrienteerApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OrienteerApplication {
 
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(OrienteerApplication.class, args);
 	}
 }
