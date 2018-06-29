691a16f920a4a0e5d6784f4c32cce5fe56b8a3e5
steven <steven.hampton415@gmail.com>
2018-06-29 11:31:07 -0500

    first commit

---
+++ src/main/java/com/orienteer/orenteer/OrenteerApplication.java
@@ -0,0 +1,12 @@
+package com.orienteer.orenteer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrenteerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrenteerApplication.class, args);
+	}
+}
