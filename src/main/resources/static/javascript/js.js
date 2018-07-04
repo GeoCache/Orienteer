5ba9dde092a0122905ed873fcad7db40892da8b2
Kelly Villalobos <kvdesign1978@gmail.com>
2018-07-03 23:23:42 -0500

    more styling, addd some shadows and fonts to html

--- src/main/resources/static/javascript/js.js
+++ src/main/resources/static/javascript/js.js
@@ -2,12 +2,19 @@
 
 
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-        var currentScrollPos = window.pageYOffset;
-        if (prevScrollpos > currentScrollPos) {
-            document.getElementById("myNavbar").style.top = "0px";
-        } else {
-            document.getElementById("myNavbar").style.top = "-56px";
-        }
-        prevScrollpos = currentScrollPos;
-    };
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("myNavbar").style.top = "0px";
+    } else {
+        document.getElementById("myNavbar").style.top = "-75px";
+    }
+    prevScrollpos = currentScrollPos;
+};
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    $(".zoom #img").css({
+        width: (100 + scroll / 5) + "%"
+    });
+});
