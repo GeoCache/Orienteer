849ffd43d0f16f9adcc627cf6f8c9d992dc711bb
Kelly Villalobos <kvdesign1978@gmail.com>
2018-07-06 16:10:34 -0500

    added Steves map code to the dashboard and it workds, commiting work

--- src/main/resources/static/javascript/js.js
+++ src/main/resources/static/javascript/js.js
@@ -1,6 +1,3 @@
-
-
-
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
@@ -23,8 +20,9 @@ new WOW().init();
 //
 // });
 // });
+// Regular map
 function regular_map() {
-    var var_location = new google.maps.LatLng(40.725118, -73.997699);
+    var var_location = new google.maps.LatLng(29.426791, -98.489602);
 
     var var_mapoptions = {
         center: var_location,
@@ -37,9 +35,42 @@ function regular_map() {
     var var_marker = new google.maps.Marker({
         position: var_location,
         map: var_map,
-        title: "New York"
+        title: "New York",
+        icon: {
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: 5,
+            strokeColor: 'black',
+            fillColor: 'red',
+            fillOpacity: 1
+        }
     });
+
+    var infoWindow = new google.maps.InfoWindow;
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            marker.setPosition(pos);
+            infoWindow.setPosition(pos);
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
 }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
+
 
 // Initialize maps
 google.maps.event.addDomListener(window, 'load', regular_map);
