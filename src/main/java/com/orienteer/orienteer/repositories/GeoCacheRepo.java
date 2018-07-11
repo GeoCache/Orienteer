5d4ef68c48a28ee2a1e56975d57d9789c5d22ed7
John <cooks.johns@gmail.com>
2018-07-11 14:01:09 -0500

    making caches

--- src/main/java/com/orienteer/orienteer/repositories/GeoCacheRepo.java
+++ src/main/java/com/orienteer/orienteer/repositories/GeoCacheRepo.java
@@ -1,8 +1,21 @@
-//package com.orienteer.orienteer.repositories;
-//
-//import com.orienteer.orienteer.models.Geocache;
-//import org.springframework.data.repository.CrudRepository;
-//
-//
-//public interface GeoCacheRepo extends CrudRepository <Geocache, long> {
-//}
+package com.orienteer.orienteer.repositories;
+
+        import com.orienteer.orienteer.models.Geocache;
+        import org.springframework.data.repository.CrudRepository;
+
+        import java.util.List;
+
+
+public interface GeoCacheRepo extends CrudRepository<Geocache, Long> {
+
+    List<Geocache> findAll();
+
+    List<Geocache> findCache(
+            long createdTime, String name, String type,
+            String locationName, String isActive, String points);
+
+    Geocache findById(long id);
+
+
+    List<Geocache> findCache(String searchCache, String searchCache1);
+}
